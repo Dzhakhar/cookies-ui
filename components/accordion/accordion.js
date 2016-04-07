596ce875d6116b1487452d10574b9eff8f38e40d
dzhakhar <dzhakhar.ukhaev@gmail.com>
2016-04-07 11:13:58 +0600

    fixup: accordion

--- components/accordion/accordion.js
+++ components/accordion/accordion.js
@@ -7,16 +7,14 @@ Object.prototype.accordion = function(obj){
 
   this.changeClass = function(e){
     if(this.many){
-      if($(e.target).hasClass("active")){
-        $(e.target).removeClass("active");
-      } else {
+      if(!$(e.target).hasClass("active")){
         $(e.target).addClass("active");
       }
     }
   }
 
   if(this.hover){
-    $(SUPER).hover(function(e){ SUPER.changeClass(e) })
+    $(SUPER).hover(function(e){ SUPER.changeClass(e) });
   } else {
     $(SUPER).click(function(e){ SUPER.changeClass(e) })
   }
