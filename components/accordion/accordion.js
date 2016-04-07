e07ad3b567fc33fb5a1caf32def2a7821caa9638
dzhakhar <dzhakhar.ukhaev@gmail.com>
2016-04-07 10:59:58 +0600

    Feature: accordion

---
+++ components/accordion/accordion.js
@@ -0,0 +1,23 @@
+Object.prototype.accordion = function(obj){
+  var SUPER = this;
+  this.many = obj.many;
+  this.hover = obj.onHover;
+
+  console.log($(this));
+
+  this.changeClass = function(e){
+    if(this.many){
+      if($(e.target).hasClass("active")){
+        $(e.target).removeClass("active");
+      } else {
+        $(e.target).addClass("active");
+      }
+    }
+  }
+
+  if(this.hover){
+    $(SUPER).hover(function(e){ SUPER.changeClass(e) })
+  } else {
+    $(SUPER).click(function(e){ SUPER.changeClass(e) })
+  }
+};
